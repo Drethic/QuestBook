20d38687b7f984d593b865524cd471f5ec5111e4
Drethic <drethic@msn.com>
2016-04-15 17:15:03 -0400

    Cleaned up config code Issue #6

--- src/main/java/drethic/questbook/config/QBConfig.java
+++ src/main/java/drethic/questbook/config/QBConfig.java
@@ -16,38 +16,26 @@ public class QBConfig {
 	public static final Logger logger = LogManager.getLogger(QuestBook.MODID);
 	
 	public static boolean spawnWithBook;
-	public static final boolean SPAWNWITHBOOK_DEFAULT = false;
-	public static final String SPAWNWITHBOOK_NAME = "spawnWithBook";
-
 	public static boolean disablePartyNotifications;
-	public static final boolean DISABLEPARTYNOTIFICATIONS_DEFAULT = false;
-	public static final String DISABLEPARTYNOTIFICATIONS_NAME = "disablePartyNotifications";
-
 	public static boolean disableQuestNotifications;
-	public static final boolean DISABLEQUESTNOTIFICATIONS_DEFAULT = false;
-	public static final String DISABLEQUESTNOTIFICATIONS_NAME = "disableQuestNotifications";
 	
 	public static final void init(FMLPreInitializationEvent e) {
 		logger.info("Loading config file.");
 		qbconfig = new Configuration(new File(e.getModConfigurationDirectory().getAbsolutePath() + "/betterquesting/questbook.cfg"));
 		qbconfig.load();
-		configOptions();
+
+		qbconfig.addCustomCategoryComment(qbconfig.CATEGORY_GENERAL, "General options for the Quest Book mod.");
+
+		spawnWithBook = configBoolOption(qbconfig.CATEGORY_GENERAL, "spawnWithBook", false, "Enable this option so new players will spawn with a Quest Book. Default: false");
+		disablePartyNotifications = configBoolOption(qbconfig.CATEGORY_GENERAL, "disablePartyNotifications", false, "[WIP]Disable party notifications.  Default: false");
+		disableQuestNotifications = configBoolOption(qbconfig.CATEGORY_GENERAL, "disableQuestNotifications", false, "[WIP]Disable quest notifications.  Default: false");
+		
 		qbconfig.save();
 	}
 	
-	public static void configOptions() {
-		final String OPTIONS = qbconfig.CATEGORY_GENERAL + qbconfig.CATEGORY_SPLITTER + "Options";
-		qbconfig.addCustomCategoryComment(OPTIONS, "General options for the Quest Book mod.");
-		Property spawnWithBookProperty = qbconfig.get(OPTIONS, SPAWNWITHBOOK_NAME, SPAWNWITHBOOK_DEFAULT);
-		spawnWithBookProperty.comment = "Enable this option so new players will spawn with a Quest Book. Default: false";
-		spawnWithBook = spawnWithBookProperty.getBoolean(SPAWNWITHBOOK_DEFAULT);
-
-		Property disablePartyNotificationsProperty = qbconfig.get(OPTIONS, DISABLEPARTYNOTIFICATIONS_NAME, DISABLEPARTYNOTIFICATIONS_DEFAULT);
-		disablePartyNotificationsProperty.comment = "[WIP]Disable party notifications.  Default: false";
-		disablePartyNotifications = disablePartyNotificationsProperty.getBoolean(DISABLEPARTYNOTIFICATIONS_DEFAULT);
-
-		Property disableQuestNotificationsProperty = qbconfig.get(OPTIONS, DISABLEQUESTNOTIFICATIONS_NAME, DISABLEQUESTNOTIFICATIONS_DEFAULT);
-		disableQuestNotificationsProperty.comment = "[WIP]Disable party notifications.  Default: false";
-		disableQuestNotifications = disableQuestNotificationsProperty.getBoolean(DISABLEQUESTNOTIFICATIONS_DEFAULT);
+	public static boolean configBoolOption(String section, String name, boolean defaultValue, String comment) {
+		Property spawnWithBookProperty = qbconfig.get(section, name, defaultValue);
+		spawnWithBookProperty.comment = comment;
+		return spawnWithBookProperty.getBoolean(defaultValue);
 	}
 }
